3f34e05d0e8eee27167535e8becb56ee28714fbe
naqashb7 <79805483+naqashb7@users.noreply.github.com>
2022-02-11 17:30:09 +0000

    Update to main method

--- FilmStars/Program.cs
+++ FilmStars/Program.cs
@@ -10,17 +10,18 @@ public class program
         Console.WriteLine("Welcome to FilmStars!");
         Console.WriteLine("Are you looking for a Film or a TV show?");
         String initialSearch = Console.ReadLine();
+        String searchResult= initialSearch.ToLower();
 
-        switch (initialSearch)
+        switch (searchResult)
         {
             case "film":
-                if( initialSearch.ToLower() == "film")
+                if( searchResult == "film")
                 {
-                    Console.WriteLine("Do you know the title of the film you would like to search for?");
+                    Console.WriteLine("Do you know the title of the show you would like to search for?");
                     String q1 = Console.ReadLine();
                     if (q1.ToLower() == "no")
                     {
-                        Console.WriteLine("Do you know the Genre of the film you would like to search for?");
+                        Console.WriteLine("Do you know the genre of the show you would like to search for?");
                         String q2 = Console.ReadLine();
                         if (q2.ToLower() == "no")
                         {
@@ -41,8 +42,30 @@ public class program
                 }
                 break;
             case "tv show":
-                if(initialSearch.ToLower()== "tv show"){
-
+                if (searchResult == "tv show")
+                {
+                    Console.WriteLine("Do you know the title of the TV show you would like to search for?");
+                    String q1 = Console.ReadLine();
+                    if (q1.ToLower() == "no")
+                    {
+                        Console.WriteLine("Do you know the Genre of the TV show you would like to search for?");
+                        String q2 = Console.ReadLine();
+                        if (q2.ToLower() == "no")
+                        {
+                            Console.WriteLine("Is there a specific director you are looking for?");
+                            String q3 = Console.ReadLine();
+                            if (q3.ToLower() == "no")
+                            {
+                                Console.WriteLine("Is there a specific actor/actress you are looking for?");
+                                String q4 = Console.ReadLine();
+                                if (q4.ToLower() == "no")
+                                {
+                                    Console.WriteLine("Is there a specific language you are looking for?");
+                                    String q5 = Console.ReadLine();
+                                }
+                            }
+                        }
+                    }
                 }
                 break;
         }
